01376e036b3f4921e770435b6debf2e79120882d
FMark161 <farkasmark1017@gmail.com>
2026-03-12 20:48:34 +0100

    első két oldal, menü, footer

---
+++ Frontend/src/pages/Events/Events.jsx
@@ -0,0 +1,13 @@
+import './Events.css';
+
+const Events = () => {
+  return (
+    <div className="events">
+      <div className="container">
+        <h1>Események</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Events;
---
+++ Frontend/src/pages/Rental/Rental.jsx
@@ -0,0 +1,13 @@
+import './Rental.css';
+
+const Rental = () => {
+  return (
+    <div className="rental">
+      <div className="container">
+        <h1>Kölcsönzés</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Rental;
