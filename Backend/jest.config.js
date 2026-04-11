826e3cb1d64b68200b6e725f9390da296568f92d
FMark161 <farkasmark1017@gmail.com>
2026-04-11 16:35:23 +0200

    jest teszt alapjainak létrehozása

---
+++ Backend/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'app/controllers/*.js',
+    'app/models/*.js',
+    '!app/models/db.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
