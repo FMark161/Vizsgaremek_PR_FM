59b132f8555afd4af9f5c6b74a852e899b42f240
FMark161 <farkasmark1017@gmail.com>
2026-04-12 11:00:23 +0200

    próba teszt

--- Frontend/selenium-tests/simple.test.js
+++ Frontend/selenium-tests/simple.test.js
@@ -1,26 +1,28 @@
-const { Builder, By } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const { Builder } = require('selenium-webdriver');
+const edge = require('selenium-webdriver/edge');
 
 describe('Egyszerű kapcsolat teszt', () => {
   let driver;
 
   beforeAll(async () => {
-    const options = new chrome.Options();
+    const options = new edge.Options();
     options.addArguments('--remote-allow-origins=*');
+    
     driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(options)
+      .forBrowser('MicrosoftEdge')
+      .setEdgeOptions(options)
       .build();
-  });
+  }, 30000);
 
   afterAll(async () => {
-    await driver.quit();
+    if (driver) {
+      await driver.quit();
+    }
   });
 
   test('A frontend elérhető', async () => {
     await driver.get('http://localhost:5173');
     const title = await driver.getTitle();
-    console.log('Oldal címe:', title);
     expect(title).toBe('Harmónia Zeneiskola');
-  });
+  }, 10000);
 });
