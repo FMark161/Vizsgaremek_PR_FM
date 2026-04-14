076d2c83a613cfe2df63dcb4e44b3b34ddbc6b33
FMark161 <farkasmark1017@gmail.com>
2026-04-14 22:11:49 +0200

    tesztelés

--- Docusaurus/.docusaurus/client-modules.js
+++ Docusaurus/.docusaurus/client-modules.js
@@ -1,7 +1,7 @@
 export default [
-  require("C:\\Users\\parao\\Desktop\\Docusaurus\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("C:\\Users\\parao\\Desktop\\Docusaurus\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\parao\\Desktop\\Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\parao\\Desktop\\Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\parao\\Desktop\\Docusaurus\\src\\css\\custom.css"),
+  require("C:\\Users\\Márk\\OneDrive\\Desktop\\Vizsgaremek_PR_FM\\Vizsgaremek_PR_FM\\Docusaurus\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\Márk\\OneDrive\\Desktop\\Vizsgaremek_PR_FM\\Vizsgaremek_PR_FM\\Docusaurus\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\Márk\\OneDrive\\Desktop\\Vizsgaremek_PR_FM\\Vizsgaremek_PR_FM\\Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\Márk\\OneDrive\\Desktop\\Vizsgaremek_PR_FM\\Vizsgaremek_PR_FM\\Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\Márk\\OneDrive\\Desktop\\Vizsgaremek_PR_FM\\Vizsgaremek_PR_FM\\Docusaurus\\src\\css\\custom.css"),
 ];
